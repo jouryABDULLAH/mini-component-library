54851bddbe4ce4db0eb9f42dbfc1c6bb893fc3eb
JouryABDULLAH <jours-222@outlook.com>
2024-08-15 01:54:50 -0700

    IconInput, missing hover and focus effect

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,19 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+
+  small:{
+    '--border-width': 1 + 'px' ,
+    '--font-size': 14 + 'px',
+  },
+  large:{
+    '--border-width': 2 + 'px' ,
+    '--font-size': 18 + 'px',
+
+  }
+};
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +26,45 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+
+  const styles = SIZES[size];
+
+
+  return <Wrapper style={styles} width={width + 'px'}>
+    <VisuallyHidden>{label}</VisuallyHidden>
+    <IconWrapper>
+     <Icon id={icon} size={size=='small'? 14 + 'px' : 18 + 'px'}></Icon>
+    </IconWrapper>
+    <Input placeholder={placeholder}></Input>
+  </Wrapper>;
 };
+
+
+const Wrapper = styled.div`
+  width: ${p=> p.width};
+  position: relative;
+  display: flex;
+  align-item: center;
+  justify-items: center;
+  border-bottom-width: var(--border-width);
+  border-bottom-color: ${COLORS.black};
+  border-bottom-style: solid;
+  font-size: var(--font-size);
+`;
+
+
+const Input = styled.input`
+  width: 100%;
+  padding: 8px 8px 8px 24px;
+  border: none;
+  font-size: inherit;
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  left: 2px;
+  bottom: 8px;
+  text-align: center;
+`
 
 export default IconInput;
